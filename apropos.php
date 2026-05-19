8b5af5061866b4caaf98648e904b3923b54d9a78
Yassine Daif <yassdaif10@gmail.com>
2026-05-19 12:54:26 +0000

    Premier Commit SEANCE09

---
+++ apropos.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description"
+        content="Ma page À propos pour démontrer que je sais utilisé GIT avec plus d'informations sur moi à propos de moi">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>À propos</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="apropos.php">À propos</a></li>
+            </ul>
+        </nav>
+    </header>
+    <h1>À propos de moi</h1>
+    <p>Bonjour, je m'appelle Yassine Daif.</p> <br>
+    <p>Je suis du groupe AB et précisément du groupe B</p>
+    <p>Mon numéro mmi est "25b06" donc "mmi25B06"</p>
+    <footer>
+        <p>&copy; 2023 Ma page. Tous droits réservés.</p>
+    </footer>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Ma page d'accueil pour démontrer que je sais utilisé GIT">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="apropos.php">À propos</a></li>
+            </ul>
+        </nav>
+    </header>
+    <h1>Ma page qui confirme que je sais utilisé GIT</h1>
+    <img src="images/cat-valid.webp" alt="Chat validé" class="hero-img">
+    <footer>
+        <p>&copy; 2023 Ma page. Tous droits réservés.</p>
+    </footer>
+</body>
+
+</html>
